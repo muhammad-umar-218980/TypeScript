c62c65f0476e73ecdd42b93ac680fc75b64b6f84
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-06-16 17:56:58 +0500

    Added initial index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function sum(a:number,b:number):number{
+    return a+b;
+}
+
+console.log(sum(1,2));
+
+function greet(name:string):string{
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("John"));
